cdfcd5cc809f1cb3e3c7769cc0b47e2ac3a57886
niyas-mojgenie <niyas@mojgenie.com>
2024-11-15 16:38:01 +0530

    category section done

--- src/context/product-context/ProductContext.jsx
+++ src/context/product-context/ProductContext.jsx
@@ -1,27 +1,38 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+import { data } from "autoprefixer";
 
 const ProductContext = createContext({});
 
 export const ProductDataProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [productCategorys, setproductCategorys] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("https://dummyjson.com/products")
-      .then((res) => {
-        setProducts(res.data.products);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  }, [products]);
+    useEffect(() => {
+        axios
+            .get("https://dummyjson.com/products")
+            .then((res) => {
+                setProducts(res.data.products);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+        axios
+            .get("https://dummyjson.com/products/categories")
+            .then((response) => {
+                setproductCategorys(response.data);
+                console.log("productCategorys :", response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching categories:", error);
+            });
+    }, []);
 
-  return (
-    <ProductContext.Provider value={products}>
-      {children}
-    </ProductContext.Provider>
-  );
+    return (
+        <ProductContext.Provider value={{ products, productCategorys }}>
+            {children}
+        </ProductContext.Provider>
+    );
 };
 
 export default ProductContext;
--- src/pages/client/HomePage.jsx
+++ src/pages/client/HomePage.jsx
@@ -5,22 +5,22 @@ import ProductCard from "../../components/client/common/ProductCard";
 import CartContext from "../../context/cart-context/CartContext";
 import { DrawerAddToCart } from "../../components/client/common/DrawerAddToCart";
 import { Footers } from "../../components/client/common/Footers";
+import ProductGallary from "../../components/client/home/ProductGallary";
 
 function HomePage() {
-  const { cart, setCart } = useContext(CartContext);
-  console.log(cart);
+    const { cart, setCart } = useContext(CartContext);
+    console.log(cart);
 
-  return (
-    <div>
-      <Nav />
-      <DrawerAddToCart />
-      <Banner />
-      <div className="grid grid-cols-4 gap-5 p-5 py-20 max-w-max mx-auto">
-        <ProductCard />
-      </div>
-      <Footers />
-    </div>
-  );
+    return (
+        <div>
+            <Nav />
+            <DrawerAddToCart />
+            <Banner />
+            <ProductCard />
+            <ProductGallary />
+            <Footers />
+        </div>
+    );
 }
 
 export default HomePage;
